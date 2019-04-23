85b35afa232c689fcb1a0d98e3eb4a3cb954f35d
Liza <liza.chambi@gmail.com>
2019-04-23 17:08:44 -0300

    Running a game with a behavior with U-Engine.

--- src/main/java/uroborosGameStudio/ui/componentListeners/btnPlayAL.java
+++ src/main/java/uroborosGameStudio/ui/componentListeners/btnPlayAL.java
@@ -4,8 +4,11 @@ import java.awt.Canvas;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import org.team.uroboros.uroboros.engine.Ability;
 import org.team.uroboros.uroboros.engine.Game;
 import org.team.uroboros.uroboros.engine.geometry.Point;
+import org.team.uroboros.uroboros.engine.input.Key;
+import org.team.uroboros.uroboros.engine.ui.Graphics;
 import org.team.uroboros.uroboros.engine.ui.RenderTexture;
 import org.team.uroboros.uroboros.engine.ui.resources.Frame;
 import org.team.uroboros.uroboros.engine.ui.resources.Sprite;
@@ -34,6 +37,43 @@ public class btnPlayAL implements ActionListener
 				Game.setScene("Escena inicial");
 				org.team.uroboros.uroboros.engine.Actor pepe = Game.createActor("Pepe");
 				pepe.learn(new RenderTexture(sprite));
+				pepe.learn(new Ability() 
+				{	
+					org.team.uroboros.uroboros.engine.Actor actor;
+					
+					@Override
+					public void onStart(org.team.uroboros.uroboros.engine.Actor actor) 
+					{
+						this.actor =actor;
+					}
+					
+					@Override
+					public void onUpdate(Double deltaTime) 
+					{
+						if(Key.UP.isPressed()) 
+						{
+							this.actor.translate(0, 3);
+						}
+						if(Key.RIGHT.isPressed()) 
+						{
+							this.actor.translate(3, 0);
+						}
+						if(Key.DOWN.isPressed()) 
+						{
+							this.actor.translate(0, -3);
+						}
+						if(Key.LEFT.isPressed()) 
+						{
+							this.actor.translate(-3, 0);
+						}
+					}
+					
+					@Override
+					public void onRender(Graphics graphics) 
+					{
+						// TODO Auto-generated method stub	
+					}
+				});
 				Game.start();
 			}
 		};
