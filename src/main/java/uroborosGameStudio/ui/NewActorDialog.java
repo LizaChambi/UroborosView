f420d6239038df0d1fcd196fc21b803fd179f9fd
Liza <liza.chambi@gmail.com>
2019-05-07 13:10:55 -0300

    Structured new actor dialog view

--- src/main/java/uroborosGameStudio/ui/NewActorDialog.java
+++ src/main/java/uroborosGameStudio/ui/NewActorDialog.java
@@ -3,40 +3,65 @@ package uroborosGameStudio.ui;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Label;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class NewActorDialog extends JDialog {
 
-	private final JPanel contentPanel = new JPanel();
+	private final JPanel globalPanel = new JPanel();
 
 	public NewActorDialog() 
 	{
 		setTitle("Nuevo Actor");
 		setBounds(100, 100, 450, 500);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		globalPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		getContentPane().add(globalPanel, BorderLayout.CENTER);
+		globalPanel.setLayout(new BorderLayout(0, 0));
 		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			JPanel errorPanel = new JPanel();
+			FlowLayout fl_errorPanel = (FlowLayout) errorPanel.getLayout();
+			fl_errorPanel.setAlignment(FlowLayout.LEFT);
+			globalPanel.add(errorPanel, BorderLayout.NORTH);
 			{
-				JButton okButton = new JButton("OK");
+				Label tittleLabel = new Label("Complete las siguientes propiedades:");
+				errorPanel.add(tittleLabel);
+			}
+		}
+		{
+			JPanel propertiesPanel = new JPanel();
+			propertiesPanel.setBorder(BorderFactory.createTitledBorder("Propiedades"));
+			globalPanel.add(propertiesPanel, BorderLayout.CENTER);
+		}
+		{
+			JPanel buttonPanel = new JPanel();
+			buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			
+			getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("Crear");
 				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
+				buttonPanel.add(okButton);
 				getRootPane().setDefaultButton(okButton);
 			}
 			
 			{
 				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) 
+					{
+						dispose();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				buttonPanel.add(cancelButton);
 			}
 		}
 	}
