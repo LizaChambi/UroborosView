2bbf9d028951aaebcfb2f846e19c71e3ab8406b6
Liza <liza.chambi@gmail.com>
2019-05-22 21:25:22 -0300

    Refactor.

--- src/main/java/uroborosGameStudio/ui/NewBehaviorDialog.java
+++ src/main/java/uroborosGameStudio/ui/NewBehaviorDialog.java
@@ -22,118 +22,191 @@ import javax.swing.JCheckBox;
 import javax.swing.JTextPane;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class NewBehaviorDialog extends JDialog 
 {
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
 	private JTextField nameTextField;
-
+	private AdmBehaviors datosDePrueba;
+	private JPanel titlePanel;
+	private JPanel configPanel;
+	private JPanel configNamePanel;
+	private JPanel textNamePanel;
+	private JPanel configDescriptionPanel;
+	private JPanel globalPanel;
+	private JPanel buttonPane;
 	
-	public NewBehaviorDialog(AdmBehaviors datosDePrueba) {
+	public NewBehaviorDialog(AdmBehaviors datosDePrueba) 
+	{
+		this.inicializeDialog(datosDePrueba);
+		this.titlePanel();
+		this.configurationPanel();
+		this.buttonPanel();
+	}
+
+	private void buttonPanel() 
+	{
+		this.inicializeButtonPanel();
+		
+		JButton okButton = new JButton("Crear");
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				
+			}
+		});
+		buttonPane.add(okButton);
+		getRootPane().setDefaultButton(okButton);
+				
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				dispose();
+			}
+		});
+		buttonPane.add(cancelButton);
+	}
+
+	private void inicializeButtonPanel() {
+		buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+	}
+
+
+	private void configurationPanel() 
+	{
+		this.inicializeConfigurationPanel();
+		
+		this.namePanel();
+		this.descriptionPanel();
+		this.globalPanel();
+	}
+
+
+	private void globalPanel() 
+	{
+		this.inicializeGlobalPanel();
+		
+		JCheckBox chbxIsGlobal = new JCheckBox("Asignar como un comportamiento global.");
+		chbxIsGlobal.setFont(new Font("Dialog", Font.PLAIN, 12));
+		globalPanel.add(chbxIsGlobal);
+	}
+
+
+	private void inicializeGlobalPanel() 
+	{
+		globalPanel = new JPanel();
+		globalPanel.setBounds(12, 142, 431, 31);
+		globalPanel.setFont(new Font("Dialog", Font.BOLD, 12));
+		FlowLayout flowLayout = (FlowLayout) globalPanel.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEADING);
+		configPanel.add(globalPanel);
+	}
+
+	private void descriptionPanel() 
+	{
+		this.inicializeDescriptionPanel();
+		
+		JLabel lblDescription = new JLabel("Ingrese una breve descripción del nuevo comportamiento:");
+		lblDescription.setFont(new Font("Dialog", Font.PLAIN, 12));
+		lblDescription.setHorizontalTextPosition(SwingConstants.LEADING);
+		configDescriptionPanel.add(lblDescription, BorderLayout.NORTH);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		configDescriptionPanel.add(textArea);
+	}
+
+	private void inicializeDescriptionPanel() 
+	{
+		configDescriptionPanel = new JPanel();
+		configDescriptionPanel.setBounds(12, 67, 424, 68);
+		configPanel.add(configDescriptionPanel);
+		configDescriptionPanel.setLayout(new BorderLayout(5, 5));
+	}
+
+	private void namePanel() 
+	{
+		this.inicializeNamePanel();
+		
+		JLabel lblName = new JLabel("Nombre:");
+		lblName.setVerticalAlignment(SwingConstants.BOTTOM);
+		configNamePanel.add(lblName, BorderLayout.WEST);
+		lblName.setFont(new Font("Dialog", Font.PLAIN, 12));
+			
+		this.inicializeTextNamePanel();
+			
+		JLabel lblNewLabel = new JLabel("Panel de errores");
+		lblNewLabel.setForeground(Color.RED);
+		lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 11));
+		textNamePanel.add(lblNewLabel);
+				
+		nameTextField = new JTextField();
+		textNamePanel.add(nameTextField);
+		nameTextField.setColumns(10);
+	}
+
+
+	private void inicializeTextNamePanel() 
+	{
+		textNamePanel = new JPanel();
+		configNamePanel.add(textNamePanel);
+		textNamePanel.setLayout(new GridLayout(0, 1, 0, 0));
+	}
+
+
+	private void inicializeNamePanel() 
+	{
+		configNamePanel = new JPanel();
+		configNamePanel.setBounds(12, 17, 424, 38);
+		configPanel.add(configNamePanel);
+		configNamePanel.setLayout(new BorderLayout(5, 0));
+	}
+
+
+	private void inicializeConfigurationPanel() 
+	{
+		configPanel = new JPanel();
+		configPanel.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Configuraci\u00F3n", TitledBorder.LEADING, TitledBorder.TOP, null, Color.DARK_GRAY));
+		contentPanel.add(configPanel);
+		configPanel.setLayout(null);
+	}
+
+
+	private void titlePanel() 
+	{
+		this.inicializeTitlePanel();
+		this.title();
+	}
+
+	private void title() 
+	{
+		JLabel lblTitle = new JLabel("Complete las siguientes propiedades del nuevo comportamiento:");
+		lblTitle.setFont(new Font("Dialog", Font.PLAIN, 12));
+		titlePanel.add(lblTitle);
+	}
+
+	private void inicializeTitlePanel() 
+	{
+		titlePanel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) titlePanel.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEADING);
+		contentPanel.add(titlePanel, BorderLayout.NORTH);
+	}
+
+	private void inicializeDialog(AdmBehaviors datosDePrueba) 
+	{
+		this.datosDePrueba = datosDePrueba;
 		setTitle("Nuevo comportamiento");
 		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout());
+		BorderLayout borderLayout = new BorderLayout();
+		getContentPane().setLayout(borderLayout);
+		contentPanel.setLayout(new BorderLayout(0, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(new BorderLayout(0, 0));
-		{
-			JPanel titlePanel = new JPanel();
-			FlowLayout flowLayout = (FlowLayout) titlePanel.getLayout();
-			flowLayout.setAlignment(FlowLayout.LEADING);
-			contentPanel.add(titlePanel, BorderLayout.NORTH);
-			{
-				JLabel lblTitle = new JLabel("Complete las siguientes propiedades del nuevo comportamiento:");
-				lblTitle.setFont(new Font("Dialog", Font.PLAIN, 12));
-				titlePanel.add(lblTitle);
-			}
-		}
-		{
-			JPanel configPanel = new JPanel();
-			configPanel.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Configuraci\u00F3n", TitledBorder.LEADING, TitledBorder.TOP, null, Color.DARK_GRAY));
-			contentPanel.add(configPanel);
-			configPanel.setLayout(new BorderLayout(5, 5));
-			{
-				JPanel configNamePanel = new JPanel();
-				configPanel.add(configNamePanel, BorderLayout.NORTH);
-				configNamePanel.setLayout(new BorderLayout(5, 0));
-				
-				JPanel namePanel = new JPanel();
-				configNamePanel.add(namePanel, BorderLayout.WEST);
-				namePanel.setLayout(new BorderLayout(0, 0));
-				{
-					JLabel lblName = new JLabel("Nombre:");
-					lblName.setHorizontalAlignment(SwingConstants.CENTER);
-					namePanel.add(lblName, BorderLayout.SOUTH);
-				}
-				
-				JPanel textNamePanel = new JPanel();
-				configNamePanel.add(textNamePanel, BorderLayout.CENTER);
-				textNamePanel.setLayout(new GridLayout(0, 1, 0, 0));
-				
-				JLabel lblNewLabel = new JLabel("Panel de errores");
-				lblNewLabel.setForeground(Color.RED);
-				lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 11));
-				textNamePanel.add(lblNewLabel);
-				
-				nameTextField = new JTextField();
-				textNamePanel.add(nameTextField);
-				nameTextField.setColumns(10);
-			}
-			{
-				JPanel configDescriptionPanel = new JPanel();
-				configPanel.add(configDescriptionPanel, BorderLayout.CENTER);
-				configDescriptionPanel.setLayout(new BorderLayout(5, 0));
-				{
-					JPanel descriptionPanel = new JPanel();
-					FlowLayout flowLayout = (FlowLayout) descriptionPanel.getLayout();
-					flowLayout.setVgap(0);
-					flowLayout.setHgap(0);
-					flowLayout.setAlignment(FlowLayout.LEADING);
-					configDescriptionPanel.add(descriptionPanel, BorderLayout.WEST);
-					{
-						JLabel lblDescription = new JLabel("Descripción:");
-						lblDescription.setHorizontalAlignment(SwingConstants.RIGHT);
-						descriptionPanel.add(lblDescription);
-					}
-				}
-				
-				JScrollPane descriptionScrollPanel = new JScrollPane();
-				configDescriptionPanel.add(descriptionScrollPanel, BorderLayout.NORTH);
-				
-				JTextArea textAreaDescription = new JTextArea();
-				descriptionScrollPanel.setViewportView(textAreaDescription);
-			}
-			{
-				JPanel globalPanel = new JPanel();
-				configPanel.add(globalPanel, BorderLayout.SOUTH);
-				FlowLayout flowLayout = (FlowLayout) globalPanel.getLayout();
-				flowLayout.setAlignment(FlowLayout.LEADING);
-				{
-					JLabel lblGlobal = new JLabel("Usar como un comportamiento global:");
-					lblGlobal.setFont(new Font("Dialog", Font.PLAIN, 12));
-					globalPanel.add(lblGlobal);
-				}
-				{
-					JCheckBox chbxIsGlobal = new JCheckBox("");
-					globalPanel.add(chbxIsGlobal);
-				}
-			}
-		}
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
 	}
 }
